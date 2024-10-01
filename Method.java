14331efc9b80b6f378385a8c3ac9f9048e69e0f6
veenabannur <veenabannur208@gmail.com>
2024-10-01 16:42:39 +0530

    test push

---
+++ Method.java
@@ -0,0 +1,18 @@
+public class Method{
+	public static void main(String[]args){
+		mul(534,456);
+		mul(43,67);
+		mul(5,78);
+		phonecall(654325678);
+	}
+	public static void mul(int Number1,int Number2){
+		int mulValue= Number1*Number2;
+	System.out.println(mulValue);
+}
+
+public static void phonecall(int phonecall){
+System.out.println("calling"+phonenumber);}
+	
+	
+
+} 
